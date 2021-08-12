9c6e015ccdbf54f0745777ab4eac63a9e04739ac
Ashley Smith <AshleyNicholeLea@outlook.com>
2021-08-12 18:00:47 -0500

    project turned in complete.Changes after completion

--- FED1StudioProject/js/toggle.js
+++ FED1StudioProject/js/toggle.js
@@ -1,14 +1,13 @@
 ﻿
 
 function toggleTheme() {
-    //Find the bootstrap link tag
+   
     let currentTheme = document.getElementById("main");
     console.log(currentTheme.getAttribute("href"));
 
-    //Check if flatly is being used currently (currentTheme)
 
     if (currentTheme.getAttribute("href").includes("Main")) {
-        //If so, replace the href with the theme of our choice (darkly)
+     
         currentTheme.setAttribute("href", "../stylesheet/Backup.css");
     }
     else {
@@ -21,7 +20,6 @@ function toggleClass() {
     let navList = document.getElementById('topnav');
     navList.classList.toggle('expanded');
 
-    //Toggle Hamburger button/Close button
     let btn = document.getElementById('menu-btn');
 
     if (navList.classList.contains('expanded')) {
@@ -32,34 +30,3 @@ function toggleClass() {
     }
 
 }
-
-//function toggleTheme() {
-//    let currentTheme = document.getElementsByClassName('switch');
-
-
-
-//}
-
-//const toggleSwitch = document.querySelector('.theme-switch input[type="check"]');
-//const currentTheme = localStorage.GetItem('main')
-
-//if (currentTheme) {
-//    document.documentElement.setAttribute('data-theme', currentTheme);
-
-//    if (currentTheme === 'main') {
-//        toggleSwitch.c
-
-//    }
-
-//}
-
-//if (localStorage.getItem("jspgtheme") != null) {
-
-//    setTheme(localStorage.getItem("jspgtheme"));
-//}
-
-
-//determine what theme i am on
-//1) Accsess the switch from html
-//2) determine if it is switched right or left
-//3) apply the correct theme to each side
